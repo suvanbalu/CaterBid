695e101975c23caf89a587b4b97a7fef79f4f511
asxwin <ashwinvelu2003@gmail.com>
2023-01-05 21:42:33 +0530

    Changes in main section

--- src/components/Mainsection.js
+++ src/components/Mainsection.js
@@ -12,34 +12,34 @@ const Mainsection = () => {
               backgroundPosition : "center",
               backgroundRepeat : "no-repeat",
           }}
-          className="w-full h-full flex flex-col justify-top items-center mb-"
-        >
+          className="w-full h-full flex flex-col justify-top items-center">
+
           <div className='flex flex-col items-center justify-center w-screen h-screen'>
           <div className="lg:w-2/3 md:mt-22 pt-4 text-white text-center border outline p-2 w-full md:w-2/3" >
             <p className="text-5xl font-bold text-phorange font-mont mb-4">{data.heading}<br /></p>
             <p className="px-4 text-2xl font-mont text-justify mb-4">{data.text}</p>
           </div>
-          <main className="px-8 pt-4 pb-8 mb-4 lg:w-2/3 lg:px-[calc(100vw/12)] h-full grid grid-cols-2 gap-6 bg-white mt-12 bg-opacity-60 rounded-lg">
+          <main className="px-8 pt-4 pb-8 mb-4 lg:w-2/3 lg:px-[calc(100vw/12)] h-fit grid grid-cols-2 gap-6 bg-white mt-12 bg-opacity-60 rounded-lg">
             <div className="flex flex-col justify-center items-center">
-              <h1 className="text-2xl p-3 tracking-wide">Are You a Caterer?</h1>
+              <h1 className="text-2xl p-3 font-mont">Are You a Caterer?</h1>
               <img
               src="https://cdn-icons-png.flaticon.com/512/1940/1940899.png"
               alt="caterer.png"
               className="h-20 mb-4"
               >
               </img>
-              <button className="p-4 bg-phorange mt-4 rounded shadow hover:text-white hover:bg-orange-600 hover:rounded-md text-black font-semibold size-5px">
+              <button className="p-4 bg-phorange font-mont mt-4 rounded shadow hover:text-white hover:bg-orange-600 hover:rounded-md text-black font-semibold size-5px">
                 Create a Caterer Profile
               </button>
             </div>
             <div className="flex flex-col items-center">
-              <h1 className="text-2xl p-3 tracking-wide">Are You a Customer?</h1>
+              <h1 className="text-2xl p-3 font-mont">Are You a Customer?</h1>
               <img 
               src="https://cdn-icons-png.flaticon.com/512/4149/4149881.png"
               alt="customer.png"
               className="h-20 mb-4"
               ></img>
-              <button className="p-4 bg-phorange mt-4 rounded shadow hover:text-white hover:bg-orange-600 hover:rounded-md text-black font-semibold size-5px">
+              <button className="p-4 bg-phorange mt-4 rounded shadow font-mont hover:text-white hover:bg-orange-600 hover:rounded-md text-black font-semibold size-5px">
               Create User Profile
               </button>
             </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,9 +19,10 @@ const Navbar = () => {
                 </img>
                 <h1 className = "text-2xl font-bold text-red-500 m-4"> CaterBid </h1>
             </div>
-            <div className='flex items-center space-x-4 px-16 '>
-                <button>How it Works</button>
-                <button>About Us</button>
+            <div className='flex items-center space-x-4 px-16'>
+                <button className='font-mont uppercase'>How it Works</button>
+                <p className='font-mont uppercase'>|</p>
+                <button className='font-mont uppercase'>About Us</button>
             </div>
             <div className='space-x-2 flex'>
                 <button className='bg-white rounded-md hover:bg-orange-100 hover:rounded-md text-black font-semibold  px-1 py-1 size-5px'>
