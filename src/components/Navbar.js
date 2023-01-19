f842f87b840fa47925640e16139848421fb402de
suvanbalu <suvanbalu@gmail.com>
2023-01-19 09:05:31 +0530

    added pagename focus

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { NavLink } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(location)
+  }, [])
+
   return (
     <nav
       style={{
         position: "sticky",
         top: 0,
       }}
-      className="bg-white mobile:px-2 mobile:my-1 sm:px-4 lg:px-[calc(100vw/20)] sm:py-1 font-sans  flex items-center sm:justify-between"
+      className="bg-white mobile:px-2 mobile:py-2 mobile:my-1 sm:px-4 lg:px-[calc(100vw/20)] sm:py-1 font-sans  flex items-center sm:justify-between"
     >
       <div className="flex items-center mobile:justify-center sm:justify-start w-1/3 mobile:space-2  sm:space-x-4 mobile:space-x-2">
         <img
@@ -17,19 +24,19 @@ const Navbar = () => {
           className="px-1 sm:h-14 mobile:h-8"
           alt="logo"
         />
-        <h1 className="sm:text-2xl mobile:text-xl font-bold text-red-500">
-          {" "}
-          CaterBid{" "}
-        </h1>
+        <Link to="/" className="sm:text-2xl mobile:text-xl font-bold text-red-500">
+          
+          CaterBid
+        </Link>
       </div>
 
       <div className="flex items-center m-4 mobile:m-1 space-x-4 mobile:pr-2 mobile:space-x-2 w-1/3 px-12 mobile:px-8  justify-center">
-        <Link to="/howitworks" className="uppercase font-mont mobile:text-xs sm:text-base hover:text-red-500">
+        <Link to="/howitworks" className={`uppercase font-mont mobile:text-xs sm:text-base ${location.pathname === "/howitworks" && "text-red-500"} hover:text-red-500`}>
           
           How it Works
         </Link>
         <p className="uppercase font-mont">|</p>
-        <button className="uppercase font-mont mobile:text-xs sm:text-base ">
+        <button className={`uppercase font-mont mobile:text-xs sm:text-base hover:text-red-500 ${location.pathname==="/AboutUs" && "text-red-500"}`}>
           <Link to="/AboutUs">About Us</Link>
         </button>
       </div>
