a523312b79a8231861d51da8129cac050d0e4b5b
suvanbalu <suvanbalu@gmail.com>
2023-01-23 15:09:11 +0530

    Small fix

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div className='w-screen h-screen bg-orange-300 flex flex-col items-center justify-center'>
-      <p className='text-3xl m-8'>Page Not Found !</p>
-      <img src = "https://cdn-icons-png.flaticon.com/512/6358/6358087.png" 
-      className='h-40'/>
-        <Link to="/home">
-        Hi
+    <div className='w-screen h-screen bg-black flex flex-col items-center  '>
+      <p className='text-3xl text-white m-8'>Page Not Found !</p>
+      <img src = "https://cdn.svgator.com/images/2022/01/404-page-design-animation.gif" 
+      className='h-1/2'/>
+        <Link to="/" className='mt-4'>
+        Return to home page
       </Link>
         
     </div>
