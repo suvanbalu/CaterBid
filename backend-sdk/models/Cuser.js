b869830f89e1968e5ed2c8412baf7132ad5d8ea4
suvanbalu <suvanbalu@gmail.com>
2023-03-10 18:23:39 +0530

    added caterer routes

--- backend-sdk/models/Cuser.js
+++ backend-sdk/models/Cuser.js
@@ -3,32 +3,25 @@ import mongoose, {Schema,model} from "mongoose";
 const schema = new Schema({
   c_username:{
     type: String,
-    required: true,
     unique: true,
   },
   comp_name :{
     type: String,
-    required: true,
   },
   b_type :{
     type: String,
-    required: true,
   },
   c_email :{
     type: String,
-    required: true,
   },
   c_password :{
     type: String,
-    required: true,
   },
   c_phone :{
     type: String,
-    required: true,
   },
   c_address :{
     type: String,
-    required: true,
   },
   c_rating:{
     type: Number,
--- backend-sdk/routes/auth.js
+++ backend-sdk/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+import Cuser from "../models/Cuser.js";
 // import { Caterer } from '../models/Cuser.js';
 
 router.get("/",(req,res)=>{
@@ -29,10 +30,47 @@ router.post("/usignup", async(req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
+// cuser signup
+router.post("/csignup", async(req, res) => {
+    console.log("1",req.body);
+    const { comp_name, b_type, c_email, c_password, c_phone, c_address, c_username } = req.body;
+    try {
+        const salt = bcrypt.genSaltSync(10)
+        const user = await Cuser.create({
+            comp_name: comp_name,
+            b_type: b_type,
+            c_email: c_email,
+            c_password: bcrypt.hashSync(c_password, salt),
+            c_phone: c_phone,
+            c_address: c_address,
+            c_username: c_username,
+        });
+        res.send(201).json({ user:user._id })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
 //  user delete
-router.delete("/delete/:id",async(req,res)=>{
+router.delete("/udelete/:id",async(req,res)=>{
     try{
         const user = await User.findOneAndDelete({username:req.params.id});
+        if(!user){
+            return res.status(404).json({error:"Not Found"});
+        }else{
+            console.log("User deleted");
+            res.status(200).json(user);
+        }
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error:err.message});
+    }
+});
+
+router.delete("/cdelete/:id",async(req,res)=>{
+    try{
+        const user = await Cuser.findOneAndDelete({c_username:req.params.id});
         if(!user){
             return res.status(404).json({error:"Not Found"});
         }else{
@@ -67,7 +105,27 @@ router.post("/ulogin",async (req,res)=>{
         console.log(err);
         res.status(500).json({error:err.message});
     }
+})
 
+// cuser login
+router.post("/clogin",async (req,res)=>{
+    try{
+        const {c_username,password} = req.body;
+        const auth = await Cuser.findOne({c_username: c_username});
+        if (auth){
+            if (bcrypt.compareSync(password,auth.c_password)){
+                const token = jwt.sign({id:auth._id},"secret-key");
+                res.status(200).json({auth:auth._id,token:token});
+            }
+        else{
+            res.status(500).json({message:"Invalid Password"});
+            }
+        }
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error:err.message});
+    }
 })
 // user secret route
 router.get("/ulogin",async (req,res)=>{
@@ -88,8 +146,24 @@ router.get("/ulogin",async (req,res)=>{
 }
 );
 
+router.get("/clogin",async (req,res)=>{
+    if (req.headers.authorization){
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token,"secret-key",(err,decoded)=>{
+            if(err){
+                res.status(401).json({error:"Invalid Token"});
+            }
+            else{
+                res.status(200).json({message:"Authorized"})
+            }
+        })
+    }
+    else{
+        res.status(401).json({error:"Token not provided"});
+    }
+});
 // user profile
-router.get("profile/:id",async(req,res)=>{
+router.get("uprofile/:id",async(req,res)=>{
     try{
         const user = await user.findOne({username:req.params.id});
         if(!user){
@@ -103,5 +177,19 @@ router.get("profile/:id",async(req,res)=>{
         res.status(500).json({error:err.message});
     }
 })
-export default router;
 
+router.get("cprofile/:id",async(req,res)=>{
+    try{
+        const user = await Cuser.findOne({c_username:req.params.id});
+        if(!user){
+            return res.status(404).json({error:"Not Found"});
+        }
+        else{
+            res.status(200).json(user);
+        }
+    }
+    catch(error){
+        res.status(500).json({error:error.message});
+    }
+})
+export default router;
