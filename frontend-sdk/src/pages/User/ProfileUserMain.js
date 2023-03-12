44572f0e9ba1249b8e8a7f532482e723ecb325c0
siveshk <siveshkannan8@gmail.com>
2023-03-12 23:44:58 +0530

    profilepage alignment change

---
+++ frontend-sdk/src/pages/User/ProfileUserMain.js
@@ -0,0 +1,310 @@
+import React, { useState, useEffect } from "react";
+import Progress_bar from "../../components/progressbar";
+import {
+  AiFillFileAdd,
+  AiFillGooglePlusCircle,
+  AiFillPlusCircle,
+  AiFillPlusSquare,
+  AiOutlineFacebook,
+  AiOutlineInstagram,
+} from "react-icons/ai";
+import { CiMail } from "react-icons/ci";
+import { SiGooglemaps } from "react-icons/si";
+import Navbar from "../../components/Navbar";
+import { useParams } from "react-router-dom";
+import BidPopup from "../../components/bidpopup";
+import Inputfield from "../../components/TextInput";
+import axios from "axios";
+
+const ProfileUserMain = () => {
+  const { id } = useParams();
+  // get user details from backend
+  const [user, setUser] = useState({
+    name: "Ashwin Veluswamy",
+    email: "",
+    location: "Tamil Nadu, India",
+    profile_pic: "",
+  });
+  // useeffect and axios to update user details
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/auth/uprofile/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        setUser(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div class="m-0 w-screen h-screen">
+      {console.log(user)}
+      <Navbar />
+      <div class="whole m-0 w-full p-2 bg-white h-full ">
+        <div class=" toppart flex items-center space-x-12 justify-center w-full bg-white m-12 ">
+          <div className=" profile_bio flex flex-col shadow w-1/3">
+            <div class="flex justify-center content-center p-5">
+              <div class="relative w-40 h-40 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                <svg
+                  class="relative text-gray-400 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+            <div class="mt-2 text-center border-b p-8">
+              <h1 class="text-4xl font-medium ">
+                {`@${id}`}
+                <span class="font-light text-gray-500"></span>
+              </h1>
+              <p class=" text-gray-800 text-2xl mt-3">{`${user.firstname} ${user.lastname}`}</p>
+              <p class="font-light text-gray-600 mt-3">Tamil Nadu, India</p>
+
+              <p class="mt-8 text-gray-500 font-semibold">Phone Number </p>
+              <p class="text-gray-500">{user.phone}</p>
+            </div>
+          </div>
+          <div class=" bg-white shadow mt-10 grid grid-rows-3 w-2/3 h-96">
+            <div class="py-5 px-24 ">
+              <p></p>
+              <Progress_bar
+                progress="100"
+                height="20px"
+                text="Totla no.of events assigned"
+                value="33"
+              />
+            </div>
+            <div class="py-5 px-24 ">
+              <Progress_bar
+                progress="66.66"
+                height="20px"
+                text="No of events successful"
+                value="20"
+              />
+            </div>
+            <div class="py-5 px-24 ">
+              <Progress_bar
+                progress="33.33"
+                height="20px"
+                text="Nof of events pending "
+                value="11"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-12 flex flex-col justify-center px-36">
+          <p class="text-gray-600 text-center font-light lg:px-16">
+            An artist of considerable range, Ryan — the name taken by
+            Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and
+            records all of his own music, giving it a warm, intimate feel with a
+            solid groove structure. An artist of considerable range.
+          </p>
+        </div>
+
+        {/* <input
+                  type="checkbox"
+                  id="my-modal-4"
+                  className="modal-toggle"
+                /> */}
+
+        {/* <label htmlFor="my-modal-4" className="modal cursor-pointer">
+                  <label className="modal-box relative max-w-3xl h-[500px]" htmlFor="">
+                    <h3 className="text-3xl font-semibold">
+                      Enter details of new event
+                    </h3>
+                    <div className="bg-white w-full rounded-xl ml-3 p-4">
+            
+
+                    <div className="mt-2">
+                      <Inputfield
+                        type="text"
+                        placeholder="Post headline :"
+                        className="border-gray-300 py-1 px-2 w-full rounded"
+                      />
+                    </div>
+                    <div className="mt-2">
+                      <Inputfield
+                        type="text"
+                        placeholder="Items needed :"
+                        className="border-gray-300 py-1 px-2 w-full rounded"
+                      />
+                    </div>
+                    <div className="mt-2">
+                      <Inputfield
+                        type="text"
+                        placeholder="Deadline :"
+                        className="border-gray-300 py-1 px-2 w-full rounded"
+                      />
+                    </div>
+                    <div className="mt-2">
+                      <Inputfield
+                        type="text"
+                        placeholder="Description :"
+                        className="border-gray-300 py-1 px-2 w-full rounded"
+                      />
+                    </div>
+                    <div className="mt-2">
+                      <Inputfield
+                        type="text"
+                        placeholder="Aprox people attending :"
+                        className="border-gray-300 py-1 px-2 w-full rounded"
+                      />
+                    </div>
+                    <div className="flex justify-end">
+                      <button class=" mr-2 mt-4  w-[90px] py-3 text-center text-white font-semibold rounded-xl hover bg-slate-600 hover:bg-slate-500 active:bg-slate-600 focus:outline-none focus:ring focus:ring-slate-500 duration-50 transition ease-in-out delay-150 ">
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                  </label>
+                </label> */}
+
+        <div class="grid grid-cols-2 space-x-4 p-12 w-full">
+          <div class="flex items-center justify-left w-[130%] h-full overflow-x-auto overflow-y-hidden scrollbar-hide pl-6 ">
+            <div class="flex justify-center h-48 w-[700px]">
+              <div className="flex justify-center gap-x-6 ml-60">
+                <div>
+                  <BidPopup
+                    num="1"
+                    name="Mr Bari Wedding"
+                    description="Mutton Briyani Yummy"
+                    src=""
+                  />
+                </div>
+                <div>
+                  <BidPopup
+                    num="2"
+                    name="Mr Ashwin Wedding"
+                    description="Sambar Rice Yummy"
+                  />
+                </div>
+                <div>
+                  <BidPopup
+                    num="3"
+                    name="Mrs Bari Wedding"
+                    description="Sambar Rice Yummy"
+                  />
+                </div>
+                <div>
+                  <BidPopup
+                    num="4"
+                    name="Mrs Ashwin Wedding"
+                    description="Mutton Briyani Yummy"
+                  />
+                </div>
+                <div>
+                  <BidPopup
+                    num="5"
+                    name="Mrs Ashwin Wedding"
+                    description="Mutton Briyani Yummy"
+                  />
+                </div>
+                <div>
+                  <BidPopup
+                    num="6"
+                    name="Mrs Ashwin Wedding"
+                    description="Mutton Briyani Yummy"
+                  />
+                </div>
+                <div>
+                  <BidPopup
+                    num="7"
+                    name="Mrs Ashwin Wedding"
+                    description="Mutton Briyani Yummy"
+                  />
+                </div>
+                <div>
+                  <BidPopup
+                    num="8"
+                    name="Mrs Ashwin Wedding"
+                    description="Mutton Briyani Yummy"
+                  />
+                </div>
+                <div>
+                  <BidPopup
+                    num="9"
+                    name="Mrs Ashwin Wedding"
+                    description="Mutton Briyani Yummy"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex justify-end">
+            <div className="p-5 mt-2 mr-20 flex bg-white shadow rounded-xl transition flex-cols items-center delay-150  hover duration-300 h-40 w-96 ">
+              <div className="ml-10  text-3xl font-semibold">Add new Event</div>
+              <label htmlFor="my-modal-4">
+                <div class=" font-bold text-gray-700 text-9xl m-auto duration-300 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105  ">
+                  <AiFillPlusCircle />
+                </div>
+              </label>
+              <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+              <label htmlFor="my-modal-4" className="modal cursor-pointer">
+                <label
+                  className="modal-box relative max-w-3xl h-[500px]"
+                  htmlFor=""
+                >
+                  <h3 className="text-3xl font-semibold">
+                    Enter details of new event
+                  </h3>
+                  <div className="bg-white w-full rounded-xl ml-3 p-4">
+                    <div className="mt-2">
+                      <Inputfield
+                        type="text"
+                        placeholder="Post headline :"
+                        className="border-gray-300 py-1 px-2 w-full rounded"
+                      />
+                    </div>
+                    <div className="mt-2">
+                      <Inputfield
+                        type="text"
+                        placeholder="Items needed :"
+                        className="border-gray-300 py-1 px-2 w-full rounded"
+                      />
+                    </div>
+                    <div className="mt-2">
+                      <Inputfield
+                        type="text"
+                        placeholder="Deadline :"
+                        className="border-gray-300 py-1 px-2 w-full rounded"
+                      />
+                    </div>
+                    <div className="mt-2">
+                      <Inputfield
+                        type="text"
+                        placeholder="Description :"
+                        className="border-gray-300 py-1 px-2 w-full rounded"
+                      />
+                    </div>
+                    <div className="mt-2">
+                      <Inputfield
+                        type="text"
+                        placeholder="Aprox people attending :"
+                        className="border-gray-300 py-1 px-2 w-full rounded"
+                      />
+                    </div>
+                    <div className="flex justify-end">
+                      <button class=" mr-2 mt-4  w-[90px] py-3 text-center text-white font-semibold rounded-xl hover bg-slate-600 hover:bg-slate-500 active:bg-slate-600 focus:outline-none focus:ring focus:ring-slate-500 duration-50 transition ease-in-out delay-150 ">
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </label>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ProfileUserMain;
